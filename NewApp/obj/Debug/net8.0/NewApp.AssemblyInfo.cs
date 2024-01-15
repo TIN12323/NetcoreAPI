c7c9b69df9703ab0e41e2e9580ef9a491bc95146
TIN12323 <hoangtin182003@gmail.com>
2024-01-15 22:14:23 +0700

    update

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5333e507e474868cbd6b7982a17703181836e21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b18260e6cb73b31fce432e3460720bdbf4ce65ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
